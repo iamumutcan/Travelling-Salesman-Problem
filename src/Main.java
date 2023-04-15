aaf1659216b065ca18f697d2bbad16211f12e921
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-04-15 18:07:12 +0300

    v12
    aktivasyon kod üretici eklendi

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,13 @@ public class Main {
     //private static final String FILE_NAME = "C:/tsp/124xy.txt";
    // private static final String FILE_NAME = "C:/tsp/5000xy.txt";
    // private static final String FILE_NAME = "C:/tsp/80000xy.txt";
-    private static final String FILE_NAME = "C:/tsp/124xy.txt";
+    private static final String FILE_NAME = "C:/tsp/5xy.txt";
     public static void main(String[] args) {
-        System.out.println(" *** Simulated Annealing *** ");
+        System.out.println(" ***       Neighbor      *** ");
+        Neighbor neighbor = new Neighbor();
+        neighbor.run(FILE_NAME);
+        System.out.println(" *************************** ");
+   /*     System.out.println(" *** Simulated Annealing *** ");
         SimulatedAnnealingAlgorithm sAA = new SimulatedAnnealingAlgorithm();
         sAA.run(FILE_NAME);
         System.out.println(" *************************** ");
@@ -19,16 +23,13 @@ public class Main {
         twoOpt.run(FILE_NAME);
         System.out.println(" *************************** ");
 
-        System.out.println(" ***       Neighbor      *** ");
-        Neighbor neighbor = new Neighbor();
-        neighbor.run(FILE_NAME);
-        System.out.println(" *************************** ");
+
 
         System.out.println(" ***     Brute Force     *** ");
         BruteForceAlgorithm bruteForce = new BruteForceAlgorithm();
         bruteForce.run(FILE_NAME);
         System.out.println(" *************************** ");
-
+*/
     }
 
 }
